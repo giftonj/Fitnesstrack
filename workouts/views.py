438b9918f7229f1dde90c13490db3ac80367727f
Gifton Njoroge <giftonnjoroge@gmail.com>
2026-01-29 09:52:39 +0300

    removing comments that are not neccessary

--- workouts/views.py
+++ workouts/views.py
@@ -5,9 +5,8 @@ from .forms import ExerciseForm
 from .models import Workout, Exercise
 
 # Create your views here.
-@login_required #It automatically checks if a user is logged in before they can access a view. If they aren't, it redirects them to the login page.
+@login_required
 def workout_list(request):
-    #workouts = Workout.objects.all().order_by('-date')
     workouts = Workout.objects.filter(user=request.user).order_by('-date')
     
     
@@ -19,31 +18,18 @@ def add_workout(request):
     ExerciseFormSet =inlineformset_factory(Workout, Exercise, form=ExerciseForm, extra=1, can_delete=True)
     
     if request.method == 'POST':
-        #workout_date =request.POST.get('date')
         
         #Get the workout date from the submited work
         workout_date = request.POST.get('date')
         
         
         #create a new unsaved workout instance
-        #workout = Workout(date=workout_date)
         workout = Workout(date=request.POST.get('date'), user=request.user)
         
         #create a formset instance with the submited data and linking it to the new workout
         formset = ExerciseFormSet(request.POST, instance=workout)
         
-        #new_workout = Workout.objects.create(date=workout_date)
-        
-        #exercise_name = request.POST.get('exercise_name')
-        #sets = request.POST.get('sets')
-        #reps = request.POST.get('reps')
-        
-        #Exercise.objects.create(
-            #workout=new_workout,
-            #name=exercise_name,
-            #sets=sets,
-            #reps=reps
-        #)
+       
         if formset.is_valid() and workout_date:
             workout.save()
             formset.save()
